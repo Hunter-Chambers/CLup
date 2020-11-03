48d69c56fb39de7ca4b5e28ce78d8379b4c5104c
hunter-chambers <hrchambers.333@gmail.com>
2020-11-03 14:24:49 -0600

    Designed basic signup page

--- clup/lib/Signup.dart
+++ clup/lib/Signup.dart
@@ -1,18 +1,91 @@
 import 'package:flutter/material.dart';
+import 'Login.dart';
 
-class Signup extends StatelessWidget{
+class Signup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(100, 107, 255, 245),
       appBar: AppBar(title: Text("This is the signup page")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            child: Text("EDITABLE TEXT FROM GOES HERE"),
-          )
-        ],
-      )
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          width: 700,
+          child: ListView(
+            padding: EdgeInsets.fromLTRB(15, 30, 15, 30),
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(bottom: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Username *",
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password *",
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email *",
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Phone Number *",
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    child: FloatingActionButton.extended(
+                      heroTag: "SubmitBtn",
+                      onPressed: () => _onButtonPressed(context, 1),
+                      label: Text(
+                        "Submit",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  _onButtonPressed(BuildContext context, int option) {
+    switch (option) {
+      case 1:
+        {
+          return Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Login(),
+              ));
+        }
+    }
   }
 }

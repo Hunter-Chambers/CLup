865578e16bcc61bf05734eeb8e3fb8ef13b90ca5
Layton Easterly <lseasterly1@buffs.wtamu.edu>
2021-01-30 13:51:41 -0600

    fixed some issues that did not merge correctly in Services

--- clup/lib/testing/Services.dart
+++ clup/lib/testing/Services.dart
@@ -1,29 +1,129 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'Employee.dart';
+import 'dart:html' show window;
 
 class Services {
-  static const ROOT =
-      "http://10.0.6.1/var/www/html/cs4391/le1010274/HelloWorld.php";
+  static const ROOT = "http://10.0.6.1/cs4391/hc998658/";
   static const _CREATE_TABLE_ACTION = "CREATE_TABLE";
-  static const _GET_ALL_ACTION = "GET_ALL";
   static const _ADD_REC_ACTION = "ADD_REC";
+  static const _GET_REC_ACTION = "GET_REC";
   static const _UPDATE_REC_ACTION = "UPDATE_REC";
   static const _DELETE_REC_ACTION = "DELETE_REC";
 
-  static Future<String> createTable() async {
+  static const _ATTEMPT_LOGIN_ACTION = "ATTEMPT_LOGIN";
+  static const _ATTEMPT_LOAD_PROFILE = "ATTEMPT_LOAD_PROFILE";
+
+  static Future<String> createTable(String tableName) async {
     try {
       var map = Map<String, dynamic>();
       map['action'] = _CREATE_TABLE_ACTION;
+      map['table_name'] = tableName;
 
-      final response = await http.post(ROOT, body: map);
+      final response = await http.post(ROOT + "database.php", body: map);
 
-      print("Create table response: ${response.body}");
+      if (response.statusCode == 200 && response.body != "error") {
+        return response.body;
+      }
 
-      return response.body;
+      return "failure";
     } catch (e) {
       print(e);
-      return "error";
+      return "failure";
+    }
+  }
+
+  static Future<String> addRec(
+      String tableName,
+      String username,
+      String password,
+      String fname,
+      String lname,
+      String email,
+      String phone) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _ADD_REC_ACTION;
+      map['table_name'] = tableName;
+      map['username'] = username;
+      map['password'] = password;
+      map['fname'] = fname;
+      map['lname'] = lname;
+      map['email'] = email;
+      map['phone'] = phone;
+
+      final response = await http.post(ROOT + "database.php", body: map);
+
+      if (response.statusCode == 200 && response.body != "error") {
+        return response.body;
+      }
+
+      return "failure";
+    } catch (e) {
+      print(e);
+      return "failure";
+    }
+  }
+
+  static Future<String> getRec(String username) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _GET_REC_ACTION;
+      map['username'] = username;
+
+      final response = await http.post(ROOT + "database.php", body: map);
+
+      if (response.statusCode == 200 && response.body != "error") {
+        return response.body;
+      }
+
+      return "failure";
+    } catch (e) {
+      print(e);
+      return "failure";
+    }
+  }
+
+  static Future<String> attemptLogin(
+      String tableName, String username, String password) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _ATTEMPT_LOGIN_ACTION;
+      map['table_name'] = tableName;
+      map['username'] = username;
+      map['password'] = password;
+
+      final response = await http.post(ROOT + "tokenization.php", body: map);
+
+      if (response.statusCode == 200 && response.body != "error") {
+        window.localStorage["csrf"] = response.body;
+        return response.body;
+      }
+
+      return "failure";
+    } catch (e) {
+      print(e);
+      return "failure";
+    }
+  }
+
+  static Future<String> attemptLoadProfile(
+      String csrfToken, String tableName) async {
+    try {
+      var map = Map<String, dynamic>();
+      map['action'] = _ATTEMPT_LOAD_PROFILE;
+      map['csrf'] = csrfToken;
+      map['table_name'] = tableName;
+
+      final response = await http.post(ROOT + "tokenization.php", body: map);
+
+      if (response.statusCode == 200 && response.body != "error") {
+        return response.body;
+      }
+      return "failure";
+    } catch (e) {
+      print(e);
+      return "failure";
     }
   }
 
@@ -32,3 +132,4 @@ class Services {
     return parsed.map<Employee>((json) => Employee.fromJson(json)).toList();
   }
 }
+

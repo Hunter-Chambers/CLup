6cb896ec3fddf1e307f5dc1747930b712b4fe6d3
Layton Easterly <lseasterly1@buffs.wtamu.edu>
2021-04-21 13:34:20 -0500

    updated layout of ASAP.dart

--- clup/lib/Schedule/ASAP.dart
+++ clup/lib/Schedule/ASAP.dart
@@ -9,6 +9,7 @@ import 'package:clup/CustomerProfile/CustomerLogin.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:clup/Schedule/StoreScheduleView.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ASAP extends StatefulWidget {
 
@@ -86,13 +87,19 @@ class _ASAPState extends State<ASAP> {
             color: Colors.white,
             height: 500,
             width: 700,
-
-            // holds all the info on the page
             child: 
               Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  Text(
+                    'Earliest Times Available',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   Container(
+                    width: 700,
                     padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
                     decoration: new BoxDecoration(
                       border: Border.all(color: Colors.black),
@@ -100,16 +107,37 @@ class _ASAPState extends State<ASAP> {
                     child: 
                       Text( this.data ),
                   ),
-                  DropdownButton(
-                    hint: Text("Select option"),
-                    value: _value,
-                    onChanged: (value) => setState(() {
-                      _value = value;
-                    }),
-                    items: storeSchedule.convertMenu(items)),
-                  FloatingActionButton.extended(
-                    label: Text('Press here!'),
-                    onPressed: () => _onButtonPressed() ),
+                  
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                    Column( 
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                      DropdownButton(
+                        hint: Text("Select option"),
+                        value: _value,
+                        onChanged: (value) => setState(() {
+                          _value = value;
+                        }),
+                        items: storeSchedule.convertMenu(items)),
+                      Container( 
+                        padding: EdgeInsets.only(top: 20),
+                        child:
+                        FloatingActionButton.extended(
+                          label: Text('Submit choice'),
+                          onPressed: () => _onButtonPressed(1) ),
+                          ),
+
+                    ],),
+                    
+                    Container(child: 
+                      FloatingActionButton.extended(
+                        label: Text('Return to Previous Page'),
+                        onPressed: () => _onButtonPressed(2) ),
+                        ),
+                  ],),
+                  
                 ],
               ),
           ),
@@ -117,88 +145,118 @@ class _ASAPState extends State<ASAP> {
       );
   }
 
-_onButtonPressed() async {
+_onButtonPressed(int option) async {
 
-  switch (_value) {
-    case 'A': {
-      // use this for testing
-      // ---------
-      //_earliestSched = '11:00 - 11:15';
-      // ---------
-      int index = storeSchedule.timeSlots.indexOf(_earliestSched);
-      storeSchedule.updateSelectedTimes(index, _earliestSched, true);
-      double offset;
+  // which button was pressed
+  switch (option) {
+    case 1: {
 
-      if ( index > 19 && index <= 39 ) {
-        offset = 320;
-      }
-      else if ( index > 39 && index <= 59 ) {
-        offset = 640;
+      // user did not select an option
+      // notify user to select an option
+      if ( _value == null ) {
+        Fluttertoast.showToast(
+          msg: 'Please select an option.',
+          gravity: ToastGravity.BOTTOM,
+          toastLength: Toast.LENGTH_LONG,
+          webPosition: 'center',
+          );
+
       }
       else {
-        offset = 960;
+        // which option was selected
+        switch (_value) {
+          case 'A': {
+            // use this for testing
+            // ---------
+            //_earliestSched = '11:00 - 11:15';
+            // ---------
+            int index = storeSchedule.timeSlots.indexOf(_earliestSched);
+            storeSchedule.updateSelectedTimes(index, _earliestSched, true);
+            double offset;
+
+            if ( index > 19 && index <= 39 ) {
+              offset = 320;
+            }
+            else if ( index > 39 && index <= 59 ) {
+              offset = 640;
+            }
+            else {
+              offset = 960;
+            }
+
+
+            return Navigator.push(context, MaterialPageRoute(
+              builder: (context) => 
+                StoreScheduleView(scheduleController: storeSchedule, 
+                                  customerController: customerProfile, 
+                                  scrollOffset: offset ),
+              )
+            );
+
+          }
+          break;
+          case 'B': {
+            String storeInfo = storeSchedule.getTextController('Store').text;
+          List<String> storeInfoSplit = storeInfo.split(', ');
+          String store = storeInfoSplit.first.split(',')[0];
+          String address = storeInfoSplit.first.split(',')[1];
+          String city = storeInfoSplit.first.split(',')[2];
+          String state = storeInfoSplit.first.split(',')[3].replaceAll("\n", '');
+          String day = storeSchedule.getTextController('day').text.toLowerCase();
+          /*
+          String storeCloseTime = 
+            storeSchedule.timeSlots[storeSchedule.timeSlots.length-1].split(' - ').last.replaceAll(":", "");
+          */
+          //String maxCapacity = '100';
+          String username = customerProfile.getTextController("username").text;
+          String contact = customerProfile.getTextController("email").text;
+          String partySize = customerProfile.getTextController("party_size").text;
+          String visitLength = storeSchedule.selectedTimes.keys.length.toString();
+          String type = 'scheduled';
+          String option = 'B';
+          String fullOrScheduledVisitStart = 'false';
+          String nextTimeBlock = _walkinStart;
+          String endTime = _walkinEnd;
+
+          String customer = json.encode({
+            username: {
+              'contact_info': contact,
+              'party_size': int.parse(partySize),
+              'type':type, 
+              'visit_length': int.parse(visitLength)
+            }
+          }).replaceAll("\"", "\\\"");
+
+            String result = await Services.makeChoice(
+                state,
+                city,
+                store,
+                address,
+                day,
+                customer,
+                option,
+                fullOrScheduledVisitStart,
+                nextTimeBlock,
+                endTime);
+
+            print(result);
+
+          }
+          break;
+
+        }
+
       }
 
-
-      return Navigator.push(context, MaterialPageRoute(
-        builder: (context) => 
-          StoreScheduleView(scheduleController: storeSchedule, 
-                            customerController: customerProfile, 
-                            scrollOffset: offset ),
-        )
-      );
-
-    }
-    break;
-    case 'B': {
-      String storeInfo = storeSchedule.getTextController('Store').text;
-    List<String> storeInfoSplit = storeInfo.split(', ');
-    String store = storeInfoSplit.first.split(',')[0];
-    String address = storeInfoSplit.first.split(',')[1];
-    String city = storeInfoSplit.first.split(',')[2];
-    String state = storeInfoSplit.first.split(',')[3].replaceAll("\n", '');
-    String day = storeSchedule.getTextController('day').text.toLowerCase();
-    /*
-    String storeCloseTime = 
-      storeSchedule.timeSlots[storeSchedule.timeSlots.length-1].split(' - ').last.replaceAll(":", "");
-    */
-    //String maxCapacity = '100';
-    String username = customerProfile.getTextController("username").text;
-    String contact = customerProfile.getTextController("email").text;
-    String partySize = customerProfile.getTextController("party_size").text;
-    String visitLength = storeSchedule.selectedTimes.keys.length.toString();
-    String type = 'scheduled';
-    String option = 'B';
-    String fullOrScheduledVisitStart = 'false';
-    String nextTimeBlock = _walkinStart;
-    String endTime = _walkinEnd;
-
-    String customer = json.encode({
-      username: {
-        'contact_info': contact,
-        'party_size': int.parse(partySize),
-        'type':type, 
-        'visit_length': int.parse(visitLength)
-      }
-    }).replaceAll("\"", "\\\"");
-
-      String result = await Services.makeChoice(
-          state,
-          city,
-          store,
-          address,
-          day,
-          customer,
-          option,
-          fullOrScheduledVisitStart,
-          nextTimeBlock,
-          endTime);
       
-      print(result);
 
     }
     break;
+    case 2: {
+
+    }
   }
+  
 
 
 }
